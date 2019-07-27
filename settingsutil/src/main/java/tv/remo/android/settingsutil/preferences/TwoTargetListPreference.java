f7d2382cbe55497787780add33d8f3f580575834
Brendon Telman <b.telman01@gmail.com>
2019-07-27 16:43:05 -0500

    Copy over classes from LR related to settings. Most of these are AOSP code, which is labeled in the file

---
+++ settingsutil/src/main/java/tv/remo/android/settingsutil/preferences/TwoTargetListPreference.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tv.remo.android.settingsutil.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+import androidx.preference.ListPreference;
+import androidx.preference.PreferenceViewHolder;
+import tv.remo.android.settingsutil.R;
+
+public class TwoTargetListPreference extends ListPreference {
+
+    @TwoTargetPreferenceHelper.IconSize
+    private int mIconSize;
+
+    public TwoTargetListPreference(Context context, AttributeSet attrs,
+                                   int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context);
+    }
+
+    public TwoTargetListPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    public TwoTargetListPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public TwoTargetListPreference(Context context) {
+        super(context);
+        init(context);
+    }
+
+    private void init(Context context) {
+        setLayoutResource(R.layout.preference_two_target);
+        final int secondTargetResId = getSecondTargetResId();
+        if (secondTargetResId != 0) {
+            setWidgetLayoutResource(secondTargetResId);
+        }
+    }
+
+    public void setIconSize(@TwoTargetPreferenceHelper.IconSize int iconSize) {
+        mIconSize = iconSize;
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        final ImageView icon = holder.itemView.findViewById(android.R.id.icon);
+        TwoTargetPreferenceHelper.handleIconSize(getContext(), icon, mIconSize);
+        TwoTargetPreferenceHelper.setupSecondTarget(holder, shouldHideSecondTarget());
+    }
+
+    protected boolean shouldHideSecondTarget() {
+        return getSecondTargetResId() == 0;
+    }
+
+    protected int getSecondTargetResId() {
+        return 0;
+    }
+}
