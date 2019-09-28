a013c5e3e1a617ec6a04ac4bdba4245202414c81
Brendon Telman <b.telman01@gmail.com>
2019-09-28 16:42:49 -0500

    Add customization of input, output, and filters for video

--- sdk/src/main/java/tv/remo/android/controller/sdk/RemoSettingsUtil.kt
+++ sdk/src/main/java/tv/remo/android/controller/sdk/RemoSettingsUtil.kt
@@ -39,8 +39,14 @@ class RemoSettingsUtil(context : Context, sharedPreferences: SharedPreferences) 
     val cameraOrientation = StringPref(context, sharedPreferences, R.string.cameraOrientationKey, "DIR_90")
     val cameraDeviceId = IntPref(context, sharedPreferences, R.string.cameraDeviceIdKey, 0)
     val cameraBitrate = StringPref(context, sharedPreferences, R.string.cameraBitrateKey, "1024")
-    val cameraFFmpegFilterOptions = StringPref(context, sharedPreferences, R.string.ffmpegFilterAddition, "")
     val useCamera2 = BooleanPref(context, sharedPreferences, R.string.useCamera2, Build.VERSION.SDK_INT >= 21)
+
+    val cameraFFmpegFilterOptions = StringPref(context, sharedPreferences,
+        R.string.ffmpegFilterAddition, "")
+    val ffmpegInputOptions = StringPref(context, sharedPreferences,
+        R.string.ffmpegInputOptionsPrefsKey, context.getString(R.string.ffmpegDefaultInputOptions))
+    val ffmpegOutputOptions = StringPref(context, sharedPreferences,
+        R.string.ffmpegOutputOptionsPrefsKey, context.getString(R.string.ffmpegDefaultOutputOptions))
 
     //microphone related settings
     val microphoneEnabled = BooleanPref(context, sharedPreferences, R.string.microphoneSettingsEnableKey, false)
--- sdk/src/main/java/tv/remo/android/controller/sdk/components/RemoVideoProcessor.kt
+++ sdk/src/main/java/tv/remo/android/controller/sdk/components/RemoVideoProcessor.kt
@@ -1,5 +1,6 @@
 package tv.remo.android.controller.sdk.components
 
+import android.content.Context
 import org.btelman.controlsdk.streaming.models.StreamInfo
 import org.btelman.controlsdk.streaming.video.processors.FFmpegVideoProcessor
 import tv.remo.android.controller.sdk.RemoSettingsUtil
@@ -9,6 +10,13 @@ import tv.remo.android.controller.sdk.RemoSettingsUtil
  */
 class RemoVideoProcessor : FFmpegVideoProcessor() {
 
+    private var streamProps: StreamInfo? = null
+
+    override fun enable(context: Context, streamInfo: StreamInfo) {
+        super.enable(context, streamInfo)
+        this.streamProps = streamInfo
+    }
+
     override fun getFilterOptions(props: StreamInfo): String {
         var filter = ""
         context?.let {
@@ -17,5 +25,52 @@ class RemoVideoProcessor : FFmpegVideoProcessor() {
             }
         }
         return super.getFilterOptions(props)+filter //intentionally no space added.
+    }
+
+    override fun getVideoInputOptions(props: StreamInfo): ArrayList<String> {
+        var command : ArrayList<String>? = null
+        RemoSettingsUtil.with(context!!){
+            it.ffmpegInputOptions.getPref().let {inputCommand ->
+                command = processCommand(inputCommand, props)
+                    ?: processCommand(it.ffmpegInputOptions.defaultValue, props)
+                    ?: throw IllegalArgumentException("Invalid input options")
+            }
+        }
+        return command!!
+    }
+
+    override fun getVideoOutputOptions(props: StreamInfo): ArrayList<String> {
+        var command : ArrayList<String>? = null
+        RemoSettingsUtil.with(context!!){
+            it.ffmpegOutputOptions.getPref().let {outputCommand ->
+                command = processCommand(outputCommand, props)
+                    ?: processCommand(it.ffmpegOutputOptions.defaultValue, props)
+                    ?: throw IllegalArgumentException("Invalid output options")
+            }
+        }
+        return command!!
+    }
+
+    /**
+     * process command to replace with camera props
+     */
+    private fun processCommand(command: String?, props: StreamInfo): ArrayList<String>? {
+        if(command.isNullOrEmpty()) return null
+        var processedCommand : String = command
+        props.apply {
+            processedCommand = processedCommand.replace("\$width", "$width")
+                .replace("\${height}", "$height")
+                .replace("\${resolution}", "${width}x$height")
+                .replace("\${framerate}", "$framerate")
+                .replace("\${bitrate}", "$bitrate")
+                .replace("\${inputStream}", "-")
+                .replace("\${endpoint}", endpoint)
+                .replace("\${bitrateflags}",
+                    "-b ${bitrate}k -minrate ${bitrate}k -maxrate ${bitrate}k -bufsize ${bitrate/1.5}k")
+        }
+
+        return ArrayList<String>().also { list ->
+            list.add(processedCommand)
+        }
     }
 }
