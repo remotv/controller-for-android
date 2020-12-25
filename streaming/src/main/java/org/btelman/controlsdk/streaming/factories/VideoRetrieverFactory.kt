4e46f36c2e3b4b12fb5b1d780230a49dafaf9a54
Brendon Telman <b.telman01@gmail.com>
2020-12-24 22:11:32 -0600

    Update some threading priorities

--- streaming/src/main/java/org/btelman/controlsdk/streaming/factories/VideoRetrieverFactory.kt
+++ streaming/src/main/java/org/btelman/controlsdk/streaming/factories/VideoRetrieverFactory.kt
@@ -1,37 +1,45 @@
 package org.btelman.controlsdk.streaming.factories
 
 import android.os.Bundle
+import org.btelman.controlsdk.models.ComponentHolder
 import org.btelman.controlsdk.streaming.models.StreamInfo
 import org.btelman.controlsdk.streaming.video.retrievers.BaseVideoRetriever
 import org.btelman.controlsdk.streaming.video.retrievers.api16.Camera1SurfaceTextureComponent
 import org.btelman.controlsdk.utils.BundleUtil
 
 object VideoRetrieverFactory {
-    fun findRetriever(bundle: Bundle): BaseVideoRetriever? {
-        BundleUtil.checkForAndInitClass(getClassFromBundle(bundle), BaseVideoRetriever::class.java)?.let {
-            return it
+    fun <T : BaseVideoRetriever> putClassInBundle(clazz: Class<T>, bundle: Bundle){
+        BundleUtil.intoBundle(BUNDLE_ID, clazz, bundle)
+    }
+
+    fun getClassFromBundle(bundle: Bundle) : Class<BaseVideoRetriever>?{
+        @Suppress("UNCHECKED_CAST")
+        (return try{
+            BundleUtil.getClassFromBundle(bundle, BUNDLE_ID) as Class<BaseVideoRetriever>
+        } catch (e : Exception){
+            e.printStackTrace()
+            null
+        })
+    }
+
+    val DEFAULT = Camera1SurfaceTextureComponent::class.java
+    const val BUNDLE_ID = "videoRetrieverClass"
+
+    fun findRetriever(bundle: Bundle): ComponentHolder<*>? {
+        val clazz = getClassFromBundle(bundle)
+        clazz?.let {
+            return ComponentHolder(clazz, bundle)
         }
         StreamInfo.fromBundle(bundle)?.also {streamInfo ->
             when {
                 streamInfo.deviceInfo.camera.contains("/dev/video") ->
                     TODO("USB Camera retriever class")
                 streamInfo.deviceInfo.camera.contains("/dev/camera") ->
-                    return Camera1SurfaceTextureComponent()
+                    return ComponentHolder(Camera1SurfaceTextureComponent::class.java, bundle)
                 streamInfo.deviceInfo.camera.contains("http") ->
                     TODO("Camera stream from other device")
             }
         }
-        return DEFAULT.newInstance()
+        return ComponentHolder(DEFAULT, bundle)
     }
-
-    fun <T : BaseVideoRetriever> putClassInBundle(clazz: Class<T>, bundle: Bundle){
-        BundleUtil.intoBundle(BUNDLE_ID, clazz, bundle)
-    }
-
-    fun getClassFromBundle(bundle: Bundle) : Class<*>?{
-        return BundleUtil.getClassFromBundle(bundle, BUNDLE_ID)
-    }
-
-    val DEFAULT = Camera1SurfaceTextureComponent::class.java
-    const val BUNDLE_ID = "videoRetrieverClass"
 }
