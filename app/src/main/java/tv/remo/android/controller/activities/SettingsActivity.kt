5f6b546fe5427952a5116f23664240d3cdefcb97
NoahAndrews <noah@revrobotics.com>
2021-01-02 22:55:13 -0600

    Switch from Kotlin Android Extensions to View Binding - patched from https://github.com/remotv/controller-for-android/commit/c39e5c1e921f38d09b99760e60529c2e8fc4a852.patch
    Signed-off-by: Brendon Telman <b.telman01@gmail.com>

--- app/src/main/java/tv/remo/android/controller/activities/SettingsActivity.kt
+++ app/src/main/java/tv/remo/android/controller/activities/SettingsActivity.kt
@@ -6,22 +6,24 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
-import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
-import kotlinx.android.synthetic.main.activity_settings.*
 import tv.remo.android.controller.R
 import tv.remo.android.controller.RemoApplication
+import tv.remo.android.controller.databinding.ActivitySettingsBinding
 import tv.remo.android.settingsutil.interfaces.SwitchBarCapableActivity
 import tv.remo.android.settingsutil.views.SwitchBar
 
 class SettingsActivity : AppCompatActivity(), NavController.OnDestinationChangedListener, SwitchBarCapableActivity {
+    private lateinit var binding: ActivitySettingsBinding
     private var first = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
-        NavigationUI.setupActionBarWithNavController(this, NavHostFragment.findNavController(nav_host_fragment))
-        NavHostFragment.findNavController(nav_host_fragment).addOnDestinationChangedListener (this)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        NavigationUI.setupActionBarWithNavController(this, Navigation.findNavController(this, R.id.nav_host_fragment))
+        Navigation.findNavController(this, R.id.nav_host_fragment).addOnDestinationChangedListener (this)
     }
 
     override fun onDestinationChanged(controller: NavController, destination: NavDestination, arguments: Bundle?) {
@@ -35,11 +37,11 @@ class SettingsActivity : AppCompatActivity(), NavController.OnDestinationChanged
     }
 
     override fun onSupportNavigateUp() : Boolean {
-        return NavHostFragment.findNavController(nav_host_fragment).navigateUp()
+        return Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp()
     }
 
     override fun getSwitchBar(): SwitchBar {
-        return switch_bar
+        return binding.switchBar
     }
 
     companion object{
