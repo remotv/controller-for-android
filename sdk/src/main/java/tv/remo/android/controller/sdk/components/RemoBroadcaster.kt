7e2932f0908dda54db75fec05ed5472a77482752
NoahAndrews <noah@revrobotics.com>
2021-01-02 22:32:59 -0600

    When using RemoBroadcaster, allow external apps to send chats and stop the stream

--- sdk/src/main/java/tv/remo/android/controller/sdk/components/RemoBroadcaster.kt
+++ sdk/src/main/java/tv/remo/android/controller/sdk/components/RemoBroadcaster.kt
@@ -6,6 +6,9 @@ import android.content.Intent
 import org.btelman.controlsdk.enums.ComponentStatus
 import org.btelman.controlsdk.hardware.interfaces.DriverComponent
 import org.btelman.controlsdk.hardware.interfaces.HardwareDriver
+import org.btelman.controlsdk.interfaces.ComponentEventListener
+import org.btelman.controlsdk.utils.InlineBroadcastReceiver
+import tv.remo.android.controller.sdk.utils.ChatUtil
 import java.nio.charset.Charset
 
 /**
@@ -17,13 +20,35 @@ import java.nio.charset.Charset
 )
 class RemoBroadcaster : HardwareDriver{
     private lateinit var context: Context
+    private var eventListener: ComponentEventListener? = null
+    private var sendChatBroadcastReceiver: InlineBroadcastReceiver? = null
+    private var stopStreamBroadcastReceiver: InlineBroadcastReceiver? = null
 
     override fun enable() {
+        sendChatBroadcastReceiver = InlineBroadcastReceiver("tv.remo.android.controller.sdk.chat.send") { _, intent: Intent? ->
+            val message = intent?.getStringExtra("message")
+            if (eventListener != null && message != null) {
+                ChatUtil.sendToSiteChat(eventListener, message)
+            }
+        }.also { it.register(context) }
 
+        stopStreamBroadcastReceiver = InlineBroadcastReceiver("tv.remo.android.controller.sdk.stream.stop") { _, _ ->
+            val stopIntent = Intent("org.btelman.controlsdk.request.stop")
+            stopIntent.`package` = context.packageName
+            context.sendBroadcast(stopIntent)
+        }.also { it.register(context) }
+
+        sendToBroadcast("start")
     }
 
     override fun disable() {
         sendToBroadcast("stop")
+        sendChatBroadcastReceiver?.unregister(context)
+        stopStreamBroadcastReceiver?.unregister(context)
+    }
+
+    override fun setEventListener(listener: ComponentEventListener?) {
+        eventListener = listener
     }
 
     override fun getStatus(): ComponentStatus {
@@ -43,7 +68,7 @@ class RemoBroadcaster : HardwareDriver{
         return true
     }
 
-    fun sendToBroadcast(data : String){
+    fun sendToBroadcast(data: String){
         val intent = Intent("tv.remo.android.controller.sdk.socket.controls").apply {
             putExtra("command", data)
         }
